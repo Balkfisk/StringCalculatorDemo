e406b38d31ef40296fd3b23a5d5ae916dfa19675
Balkfisk <31304089+Balkfisk@users.noreply.github.com>
2017-12-12 15:06:55 +0100

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculatorDemo
+{
+    public class Calculator
+    {
+        /// <summary>
+        /// Calculate sum
+        /// </summary>
+        /// <param name="expression">Digits to add as string</param>
+        /// <returns>Sum</returns>
+        public int Add(string expression)
+        {
+            if (string.IsNullOrEmpty(expression)
+            {
+                return 0;
+            }
+            int sum = int.Parse(expression);
+            return sum;
+
+
+        }
+    }
+}
